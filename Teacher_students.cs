5036cb008842ba1b86478441ee6630f2edb4353a
ayshamol <36288332+ayshamol@users.noreply.github.com>
2018-12-02 04:16:39 -0800

    Add files via upload

---
+++ Teacher_students.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ChatBot
+{
+    public partial class Teacher_students : Form
+    {
+        BaseConnection con = new BaseConnection();
+        public Teacher_students()
+        {
+            InitializeComponent();
+            fillgrid();
+        }
+        public void fillgrid()
+        {
+            try
+            {
+
+
+                string query1 = "select * from Student_details where department=(select department from Teacher_details where userid='"+Program.userid+"')";
+                DataSet ds1 = con.ret_ds(query1);
+                dataGridView2.DataSource = ds1.Tables[0].DefaultView;
+
+               
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("exception occured....");
+            }
+        }
+    }
+}
